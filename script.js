380d8f27b71a82bb4d1ce8f5c3d028a740037e4a
piyyadav06 <99813609+piyyadav06@users.noreply.github.com>
2022-05-30 09:34:34 +0530

    It's an webpage creed by me
    initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+
+let btn =document.getElementById('btn');
+btn.addEventListener('click',function(){
+    this.innerHTML="Lawda lele mera";
+})
+let btn1 =document.getElementById('btn1');
+btn1.addEventListener('click',function(){
+    this.innerHTML="gand me dum he be tor";
+})
